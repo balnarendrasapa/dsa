c104ac3a300ad32cec303990054ddf0c82e53808
Bal Narendra Sapa <61614290+balnarendrasapa@users.noreply.github.com>
2025-06-12 11:36:18 -0400

    Create 405. Convert a Number to Hexadecimal.py

---
+++ leetcode/405. Convert a Number to Hexadecimal.py
@@ -0,0 +1,9 @@
+class Solution:
+    def toHex(self, num: int) -> str:
+        if num == 0:
+            return "0"
+            
+        if num < 0:
+            num += 2**32
+
+        return hex(num)[2:]
