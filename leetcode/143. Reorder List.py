cd7c021be6dcce3b22fa740d42aa0b421fc98331
Bal Narendra Sapa <61614290+balnarendrasapa@users.noreply.github.com>
2025-07-12 13:05:15 -0400

    Create 143. Reorder List.py

---
+++ leetcode/143. Reorder List.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev = None
+        temp = slow.next
+        slow.next = None
+        while temp:
+            temp1 = temp.next
+            temp.next = prev
+            prev = temp
+            temp = temp1
+
+        first, second = head, prev
+        while second:
+            tmp1 = first.next
+            tmp2 = second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
+        
