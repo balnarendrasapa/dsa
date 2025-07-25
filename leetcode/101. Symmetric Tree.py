df390a4426f4c2f9c1a30fa27122b79e1664c74f
Bal Narendra Sapa <61614290+balnarendrasapa@users.noreply.github.com>
2025-07-25 16:57:05 -0400

    Create 101. Symmetric Tree.py

---
+++ leetcode/101. Symmetric Tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def checkSymmetry(left, right):
+
+    if not left and not right:
+        return True
+    
+    if not left or not right:
+        return False
+
+    if left.val != right.val:
+        return False
+
+    return checkSymmetry(left.left, right.right) and checkSymmetry(left.right, right.left)
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        return checkSymmetry(root.left, root.right)
