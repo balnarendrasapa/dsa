530b85cf7a31eb824cfb2dcc8a95a886622ac764
Bal Narendra Sapa <61614290+balnarendrasapa@users.noreply.github.com>
2025-08-03 12:20:30 -0400

    Create 57. Insert Interval.py

---
+++ leetcode/57. Insert Interval.py
@@ -0,0 +1,17 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        intervals.append(newInterval)
+        intervals.sort(key = lambda x: x[0])
+
+        merged = [intervals[0]]
+
+        for curr in intervals[1:]:
+            prev = merged[-1]
+
+            if curr[0] <= prev[1]:
+                prev[1] = max(prev[1], curr[1])
+            else:
+                merged.append(curr)
+
+        return merged
