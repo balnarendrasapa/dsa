af9f463f7aab9994cae3cd1e83653cc86841e4c2
Bal Narendra Sapa <61614290+balnarendrasapa@users.noreply.github.com>
2025-08-09 18:05:55 -0400

    Create 416. Partition Equal Subset Sum.py

---
+++ leetcode/416. Partition Equal Subset Sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2 == 1:
+            return False
+
+        target = sum(nums) // 2
+
+        dp = [False for i in range(target + 1)]
+        dp[0] = True
+
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
+
+        return dp[-1]
