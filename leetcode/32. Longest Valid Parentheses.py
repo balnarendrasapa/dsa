6df72cdcda640d8a15d4894326da265a8ad35181
Bal Narendra Sapa <61614290+balnarendrasapa@users.noreply.github.com>
2025-08-09 22:21:45 -0400

    Create 32. Longest Valid Parentheses.py

---
+++ leetcode/32. Longest Valid Parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        
+        stack = [-1]
+        res = 0
+
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+
+            elif s[i] == ")":
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    res = max(res, i - stack[-1])
+
+
+        return res
