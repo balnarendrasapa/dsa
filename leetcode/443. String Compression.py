9ccd67733385e3fe291c52eb599e3afe0ff25069
Bal Narendra Sapa <61614290+balnarendrasapa@users.noreply.github.com>
2025-08-11 10:47:26 -0400

    Create 443. String Compression.py

---
+++ leetcode/443. String Compression.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        if len(chars) == 1:
+            return 1
+
+        read = 0
+        write = 0
+
+        while read < len(chars):
+            char = chars[read]
+
+            count = 0
+            while read < len(chars) and chars[read] == char:
+                read += 1
+                count += 1
+
+            chars[write] = char
+            write += 1
+
+            if count > 1:
+                for c in str(count):
+                    chars[write] = c
+                    write += 1
+
+        return write
