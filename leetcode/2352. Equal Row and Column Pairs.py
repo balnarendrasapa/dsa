7eaead9e340f7a0e1f84ff798e052afc49ee067d
Bal Narendra Sapa <61614290+balnarendrasapa@users.noreply.github.com>
2025-08-12 11:34:14 -0400

    Update 2352. Equal Row and Column Pairs.py

--- leetcode/2352. Equal Row and Column Pairs.py
+++ leetcode/2352. Equal Row and Column Pairs.py
@@ -15,3 +15,25 @@ class Solution:
                         count += 1
 
         return count
+
+# class Solution:
+#     def equalPairs(self, grid: List[List[int]]) -> int:
+#         mp1=defaultdict(int)
+#         mp2=defaultdict(int)
+
+
+#         for r in grid:
+#             mp1[tuple(r)]+=1
+        
+#         for i in range(len(grid)):
+#             for j in range(i+1,len(grid[0])):
+#                 grid[i][j],grid[j][i]=grid[j][i],grid[i][j]
+        
+#         for c in grid:
+#             mp2[tuple(c)]+=1
+        
+#         count=0
+#         for row in mp1:
+#             if row in mp2:
+#                 count+=mp1[row]*mp2[row]
+#         return count
