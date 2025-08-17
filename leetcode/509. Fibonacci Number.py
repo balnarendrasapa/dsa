60c641a0b9f5a51e607b8f40c1a2f87b0dc5be91
Bal Narendra Sapa <61614290+balnarendrasapa@users.noreply.github.com>
2025-08-17 16:29:11 -0400

    Create 509. Fibonacci Number.py

---
+++ leetcode/509. Fibonacci Number.py
@@ -0,0 +1,24 @@
+memo = {}
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        
+        if n <= 2:
+            return 1
+
+        if n in memo:
+            return memo[n]
+
+        dp = [0] * (n + 1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        memo[n] = dp[n]
+
+        return dp[n]
